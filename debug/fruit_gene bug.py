3adad9fcffdfd4fc6e7fa089d9e0833e19686c0f
shuawoo <119118131+shuawoo@users.noreply.github.com>
2022-12-04 02:17:24 +0100

    Delete debug directory

--- debug/fruit_gene bug.py
+++
@@ -1,6 +0,0 @@
-def creat(self):
-    self.pos = Point(row=random.randint(1, ROW - 1), col=random.randint(1, COL - 1))
-    for i in self.snakes.body:
-        if i.row == self.pos.row and i.col == self.pos.col:
-            self.creat()
-            pass
