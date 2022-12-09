f1adec4acbc86952e921902c857886a71fd90fa0
shuawoo <119118131+shuawoo@users.noreply.github.com>
2022-12-09 15:58:32 +0100

    split

---
+++ V2/draw.py
@@ -0,0 +1,20 @@
+import pygame
+
+
+class Point:
+    row = 0
+    col = 0
+
+    def __init__(self, row, col):
+        self.row = row
+        self.col = col
+
+    def copy(self):
+        return Point(row=self.row, col=self.col)
+
+
+def rect(point, color):
+    from main import scrn, cell
+    left = point.col * cell
+    top = point.row * cell
+    pygame.draw.rect(scrn, color, (left, top, cell, cell))
